60402f1e91901edd4f3bd05e0fdcdd9b331795ac
Cosmo-Wen <95605718+Cosmo-Wen@users.noreply.github.com>
2022-08-17 02:11:39 +0800

    home page without api done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { useState } from 'react';
 import styled from '@emotion/styled';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
@@ -6,32 +8,55 @@ import Button from '@mui/material/Button';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Container = styled.div `
-    font: "Roboto";
     background-color: #ededed;
     height: 100%;
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
 `;
 
 const Home = () => {
+    const [username, setUsername] = useState("");
+    
+    const handleClick = () => {
+        console.log("Next");
+    };
+    
+    const handleTextChange = (event) => {
+        setUsername(event.target.value);
+    };
+
     return (
         <Container>
-            <GitHubIcon />
-            <Paper elevation = {0} variant = "outlined">
+            <Box padding = {2}>
+                <GitHubIcon sx = {{fontSize: 50}} />
+            </Box>
+            <Paper variant = "outlined">
                 <Box padding = {2}>
-                    <body style={{fontFamily: "Roboto", fontSize: "36px"}}>Github Repository Login</body>
+                    <body style={{fontFamily: "Roboto", fontSize: "30px"}}>Github Repository Login</body>
                 </Box>
-                <Box padding = {1} sx = {{allignItmes: "center"}}>
+                <Box padding = {1}>
                     <TextField
-                        id="outlined-required"
-                        label="Username"
+                        id = "outlined-required"
+                        label = "Username"
+                        helperText = "Please enter your username to search for Repos"
+                        sx = {{display: "flex", justifyContent: "center", marginBottom: "10px", marginX: "20px"}}
+                        value = {username}
+                        onChange = {handleTextChange}
+                        onKeyPress = {(e) => {
+                            if (e.key === "Enter") {
+                                console.log("Next");
+                            }
+                        }}
                     />
-                    <Button variant="contained" color = "success">Sign In</Button>
+                </Box>
+                <Box sx = {{display: "flex", justifyContent: "center", marginBottom: "20px"}}>
+                    <Button variant="contained" color = "success" onClick = {handleClick}>Search</Button>
                 </Box>
             </Paper>
         </Container>
     )
-}
+};
 
 export default Home;
