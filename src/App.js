3096abdaa91a82defbbd66b22dc823b9066710e4
Cosmo-Wen <95605718+Cosmo-Wen@users.noreply.github.com>
2022-08-17 19:25:56 +0800

    repo content components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Content from "./pages/Content.js";
 import Home from "./pages/Home.js"
 import Repos from "./pages/Repos.js"
+
 
 function App() {
     return (
         <BrowserRouter>
         <Routes>
-            <Route path = "/" element = {<Repos />} />
+            <Route path = "/" element = {<Home />} />
         </Routes>
         </BrowserRouter>
     );
---
+++ src/pages/Content.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Paper from '@mui/material/Paper'
+import Link from '@mui/material/Link';
+import styled from '@emotion/styled';
+
+const Container = styled.div `
+    background-color: #ededed;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-family: Roboto
+`;
+
+const Content = () => {
+    return (
+        <Container>
+            <Paper variant = "outlined" sx = {{width: 600, height: 600}}>
+                <Link href="https://github.com/" underline="hover">
+                    <h1>Repo Name</h1>
+                </Link>
+                <body1> Description </body1>
+            </Paper>
+        </Container>
+    );
+};
+
+export default Content;
